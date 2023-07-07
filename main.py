6038f356f67fcaa9a7cea1b0a3e866ca7152427a
Miguel Lopez <ing.miguell@gmail.com>
2023-07-07 15:39:25 -0600

    agrego paleta de colores, titulos e icono

--- main.py
+++ main.py
@@ -6,8 +6,18 @@ pantalla = pygame.display.set_mode(
     size=(800, 600)
 )
 
+pygame.display.set_caption(
+    title="Space Invaders"
+)
+
+icono = pygame.image.load("ovni.png")
+pygame.display.set_icon(icono)
+
 ejecutar = True
 while ejecutar:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             ejecutar = False
+    
+    pantalla.fill((34, 87, 122))
+    pygame.display.update()
