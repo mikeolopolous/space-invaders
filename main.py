c3c427197c6bb2d26314cb2188519908bffdb7cc
Miguel Lopez <ing.miguell@gmail.com>
2023-07-07 17:19:41 -0600

    agrego y posiciono al jugador respecto a la pantalla del juego

--- main.py
+++ main.py
@@ -13,11 +13,24 @@ pygame.display.set_caption(
 icono = pygame.image.load("ovni.png")
 pygame.display.set_icon(icono)
 
+img_jugador = pygame.image.load("cohete.png")
+pos_x_jugador = 368
+pos_y_jugador = 536
+
+
+def jugador():
+    pantalla.blit(
+        source=img_jugador,
+        dest=(pos_x_jugador, pos_y_jugador)
+    )
+
+
 ejecutar = True
 while ejecutar:
+    pantalla.fill((34, 87, 122))
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             ejecutar = False
     
-    pantalla.fill((34, 87, 122))
+    jugador()
     pygame.display.update()
