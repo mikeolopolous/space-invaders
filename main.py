e12716891fb9f5eff1293dc8f890aa58ae0baf00
Miguel Lopez <ing.miguell@gmail.com>
2023-07-10 09:33:27 -0600

    agrego movimiento al jugador

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ pygame.display.set_icon(icono)
 img_jugador = pygame.image.load("cohete.png")
 pos_x_jugador = 368
 pos_y_jugador = 536
+pos_x_cambia = 0
 
 
 def jugador(x, y):
@@ -31,6 +32,18 @@ while ejecutar:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             ejecutar = False
+
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_LEFT:
+                pos_x_cambia = -2.5
+        
+            if evento.key == pygame.K_RIGHT:
+                pos_x_cambia = 2.5
+
+        if evento.type == pygame.KEYUP:
+            if evento.key == pygame.K_LEFT or evento.key == pygame.K_RIGHT:
+                pos_x_cambia = 0
     
+    pos_x_jugador += pos_x_cambia
     jugador(pos_x_jugador, pos_y_jugador)
     pygame.display.update()
